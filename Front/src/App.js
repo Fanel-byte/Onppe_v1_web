cc8f4250d0d7a90b5dac76412cfc0e0e55d6a9d1
mosef-hmt <m.hamitouche@esi-sba.dz>
2023-05-08 12:15:21 +0100

    add code

---
+++ Front/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Template from './components/Template';
+import Details from './pages/details';
+
+function App () {
+  return (
+    <Template>
+      {/* Content goes here */}
+     <Details/>
+    </Template>
+  );
+};
+
+export default App;
