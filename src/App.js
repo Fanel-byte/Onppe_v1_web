99049a3e509f2eb4152d2920b9ffa6929f7517a2
fanel_byte <jf_bentaleb@esi.dz>
2023-05-23 07:16:27 +0100

    added main and details

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Template from './components/Template';
 import Details from './pages/details';
+import Main from './pages/main';
+
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+
 
 
 const App = () => {
   return (
-   
-    <Template>
-      {/* Content goes here */}
-     
-     <Details/>
-    </Template>
+<div className="flex h-screen flex-row-reverse">
+      {/* Side Menu */}
+      <div className="w-1/6">
+        <Navbar />
+      </div>
+      {/* Content */}
+      <div className="flex-1 relative overflow-y-auto">
+        {/* Header */}
+        <div className="bg-white shadow-sm">
+          <Header />
+        </div>
+
+        <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/details" element={<Details />} />
+        {/* Other routes go here */}
+      </Routes>
+      </div>
+    </div>
+
+
+
 
   );
 };
