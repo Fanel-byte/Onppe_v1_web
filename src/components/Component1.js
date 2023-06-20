680b3b5e235f438e961b598ac43b998014efd541
Walid Belguenbour <62221322+walidbelguenbour@users.noreply.github.com>
2023-06-20 22:38:45 +0200

    Add files via upload

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,24 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const Component1 = () => {
-  const [signalements, setSignalements] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:4000/signalement/")
-      .then(response => {
-        setSignalements(response.data.map(signalement => {
-          return {
-            hour: signalement.date, // remplacez "hour" par le nom de la propriété correspondant à l'heure dans votre modèle de données
-            count: signalement.nombre // remplacez "count" par le nom de la propriété correspondant au nombre de signalements dans votre modèle de données
-          };
-        }));
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
+  const [signalements, setSignalements] = useState([
+    { hour: '00h', count: 12 },
+    { hour: '01h', count: 15 },
+    { hour: '02h', count: 10 },
+    { hour: '03h', count: 18 },
+    { hour: '04h', count: 20 },
+    { hour: '05h', count: 28 },
+    { hour: '06h', count: 22 },
+    { hour: '07h', count: 35 },
+    { hour: '08h', count: 42 },
+    { hour: '09h', count: 38 },
+    { hour: '10h', count: 30 },
+    { hour: '11h', count: 25 },
+    { hour: '12h', count: 28 },
+    { hour: '13h', count: 35 },
+    { hour: '14h', count: 40 },
+    { hour: '15h', count: 45 },
+    { hour: '16h', count: 48 },
+    { hour: '17h', count: 52 },
+    { hour: '18h', count: 50 },
+    { hour: '19h', count: 45 },
+    { hour: '20h', count: 40 },
+    { hour: '21h', count: 32 },
+    { hour: '22h', count: 28 },
+    { hour: '23h', count: 20 }
+  ]);
 
   return (
     <div style={{ 
@@ -30,15 +39,7 @@ const Component1 = () => {
       borderRadius: 'mixed', 
       padding: '20px',
     }}>
-      <h2 style={{ 
-        fontFamily: 'Montserrat', 
-        fontWeight: 'bold',
-        width: '133px', 
-        height: '16px', 
-        fontSize: '13px', 
-        color: '#ED254E',
-        textAlign: 'center', 
-      }}>معدل الاخطارات اليومي</h2>
+      <h2>معدل الاخطارات اليومي</h2>
 
       <ResponsiveContainer width="100%" height="100%">
         <LineChart data={signalements} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
